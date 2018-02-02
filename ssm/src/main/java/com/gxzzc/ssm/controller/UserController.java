fd082a1cc8b16fc4d79e17ac63ee52c99083d0a2
qingyang9527 <2419744266@qq.com>
2018-02-02 19:19:14 +0800

    第一次提交

---
+++ ssm/src/main/java/com/gxzzc/ssm/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.gxzzc.ssm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * @author HM 2018年1月12日
+ */
+public class UserController implements Controller{
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.mvc.Controller#handleRequest(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
